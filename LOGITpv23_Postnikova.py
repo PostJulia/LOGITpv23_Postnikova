114c20353fd95354520dd73e06e81906c2848660
opilane <opilane@HP-CZC2349HV0>
2023-12-11 11:31:11 +0200

    Ülesanned 1-5

--- LOGITpv23_Postnikova.py
+++ LOGITpv23_Postnikova.py
@@ -1,3 +1,52 @@
+#6
+from random import *
+from math import *
+a=float(input("pikkus: "))
+b=float(input("laius: "))
+d=sqrt(a**2+b**2)
+print("Diagonaal=",d,"m")
+
+
+
+
+
+#5
+from random import *
+from math import *
+u=float(input("Ümbermõtt: ")) #L=pi*2*r=pi*d
+d=round(u/pi,2)
+print("Läbimõõt =",d)
+
+
+
+
+
+#4
+from random import *
+kokku=randint(1,100)
+print("Laual peal on",kokku,"kommid. Mitu tahad süüa?")
+kommid=int(input())
+kokku-=kommid
+print("Nüüd kokku on",kokku)
+
+
+
+
+#3
+vanus=18
+eesnimi="Jaak"
+pikkus=16.5
+kas_käib_koolis=True
+
+print("Muutuja vanus=",vanus,"on", type(vanus))
+print("Muutuja eesnimi=",eesnimi,"on", type(eesnimi))
+print("Muutuja pikkus=",pikkus,"on", type(pikkus))
+print("Muutuja kas_käib_koolis=",kas_käib_koolis,"on", type(kas_käib_koolis))
+
+
+
+
+#2
 arv1=float(input("Arv1: "))
 t=input("Tehe: ")
 arv2=float(input("Arv2: "))
@@ -9,7 +58,7 @@ print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
 
 
 
-
+#1
 print("Tere maailm!".center(75,"-"))
 nimi=input("Mis on sinu nimi? ").capitalize() #python->Python
 print("Tere "+nimi+"!")
