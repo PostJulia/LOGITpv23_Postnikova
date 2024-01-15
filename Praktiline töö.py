925dee2f4598664743d2619b9cdbab772afd3898
PostJulia <153494094+PostJulia@users.noreply.github.com>
2024-01-15 10:14:17 +0200

    Praktiline töö.py

--- Praktiline töö.py
+++ Praktiline töö.py
@@ -2,31 +2,34 @@ print("Tere! Olen sinu uus sõber - Python!")
 nimi = input("Mis on sinu nimi?: ").capitalize()
 print(nimi + ", oi kui ilus nimi!")
 vastus = input(nimi + "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => ")
-if vastus == "0":
+if vastus == "1":
    try:
-        if vastus == "1":
-            pikkus = int(input("Sisesta oma pikkus (cm): "))
-            mass = float(input("Sisesta oma kaal (kg): "))
-            indeks = mass / (0.01 * pikkus) ** 2
-            print(nimi + ", Sinu keha indeks on: ".format(indeks))
-            if indeks < 16:
-              print("Tervisele ohtlik alakaal")
-            elif 16 <= indeks < 19:
-               print("Alakaal")
-            elif 19 <= indeks < 25:
-               print("Normaalkaal")
-            elif 25 <= indeks < 30:
-               print("Ülekaal")
-            elif 30 <= indeks < 35:
-               print("Rasvumine")
-            elif 35 <= indeks < 40:
-               print("Tugev rasvumine")
-            else:
-               print("Tervisele ohtlik rasvumine")
-   except :
+       pikkus = int(input("Sisesta oma pikkus (cm): "))
+       mass = float(input("Sisesta oma kaal (kg): "))
+       indeks = mass/(0.01 * pikkus)**2
+       print(nimi + ", Sinu keha indeks on: "+ str(indeks))
+       if indeks < 16:
+           print("Tervisele ohtlik alakaal")
+       elif 16 <= indeks < 19: 
+           print("Alakaal")
+       elif 19 <= indeks < 25:
+           print("Normaalkaal")
+       elif 25 <= indeks < 30:
+           print("Ülekaal")
+       elif 30 <= indeks < 35:
+           print("Rasvumine")
+       elif 35 <= indeks < 40:
+           print("Tugev rasvumine")
+       else:
+           print("Tervisele ohtlik rasvumine")
+   except ValueError:
        print("Kohtumiseni, " + nimi + "! Igavesti Sinu, Python!")
 else:      
    print("Kahju! See on väga kasulik info!")
+
+
+
+
 
 #from random import *
 #from datetime import *
@@ -40,12 +43,6 @@ else:
 #print(d.isnumeric()) #True "11111" #False "1.1"
 #print(d.isdigit()) #True "11111" #False "1.1"
 #print(d.isdecimal()) #True "11111" #False "1.1"
-
-
-
-
-
-
 
 
 
