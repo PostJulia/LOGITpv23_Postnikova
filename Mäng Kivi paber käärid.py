6a9acdd7685f8ddcac88db5e399ff96553b16ef3
opilane <opilane@HP-CZC2349HT9>
2024-02-26 10:44:50 +0200

    Funktsioonid

--- Mäng Kivi paber käärid.py
+++ Mäng Kivi paber käärid.py
@@ -1,15 +1,20 @@
-from random import *
-spisok=[]
-b=randint("kivi","käärid","paber")
-v=int(input("Kas valida kivi, käärid või paber?")).lower()
-if:
-    pass
-elif :
-    pass
-else:
-    pass
+import random
+while True:
+    v1=input("Mängija 1, vali märk (kivi/käärid/paber): ").lower()
+    v2=random.choice(["kivi", "käärid", "paber"])
+    print(v2)
+    
+    if v1 not in ["kivi", "käärid", "paber"]:
+        print("Vale valik. Palun vali uuesti.")
+        continue
 
-
-
-
-
+    if v1==v2:
+        print("Viik!")
+    elif (v1=="kivi" and v2=="käärid") or (v1=="käärid" and v2=="paber") or (v1=="paber" and v2=="kivi"):
+        print("Mängija 1 võidab!")
+    else:
+        print("Mängija 2 võidab!")
+        
+    kordus=input("Kas soovite veel mängida? (jah/ei): ").lower()
+    if kordus !="jah":
+        break
