120240ca13e03e72fd8a7ae6277d48f5bcf0c32f
opilane <opilane@HP-CZC2349HT9>
2024-04-30 09:23:40 +0300

    1234

--- Wordle graafilise kujundiga/Wordle graafilise kujundiga/Wordle_graafilise_kujundiga.py
+++ Wordle graafilise kujundiga/Wordle graafilise kujundiga/Wordle_graafilise_kujundiga.py
@@ -29,32 +29,30 @@ def liidest(arvamine, tulemus):
 
 def saatmine():
     global üritus, hetke
-    arvamine=arvamine_muutuja.get().upper()
-    if len(arvamine)!=5:
-        tulemus.config(text="Sõna peab olema 5 tähemärki pikk!")
-        return
+    arvamine=[sisestus.get().upper() for sisestus in sisestused[üritus]]
     
     tulemus=kontrolli(arvamine, hetke)
     liidest(arvamine, tulemus)
 
-    if arvamine==hetke or üritus==5:
-        if arvamine!=hetke:
-            tulemus.config(text=f"Õige sõna: {hetke}", fg="#ff0000")
+    if arvamine==list(hetke) or üritus==5:
+        if arvamine!=list(hetke):
+            silt.config(text=f"Õige sõna: {''.join(hetke)}", fg="#ff0000")
         else:
-            tulemus.config(text="")
+            silt.config(text="")
         for sisestusrida in sisestused:
             for sisestus in sisestusrida:
                 sisestus.config(state='disabled')
     else:
         üritus+=1
-        silt.config(text=f"Jäänud püüdeid: {6 - üritus}")
-        arvamine_muutuja.set("")
+        jäänud.config(text=f"Jäänud püüdeid: {6 - üritus}")
+        for sisestus in sisestused[üritus]:
+            sisestus.focus_set()
 
 def algus():
     global hetke, üritus
     hetke=vali(sõnad)
     üritus=0
-    arvamine_muutuja.set("")
+    jäänud.config(text="")
     silt.config(text="")
 
     for sisestusrida in sisestused:
@@ -68,7 +66,7 @@ hetke=vali(sõnad)
 
 aken=tk.Tk()
 aken.title("Wordle")
-aken.geometry("400x800")  
+aken.geometry("500x800")  
 aken.config(bg='#87ceeb')
 
 põhiraam=tk.Frame(aken, bg="#87ceeb")
@@ -79,18 +77,16 @@ for i, rida in enumerate(sisestused):
     for j, sisestus in enumerate(rida):
         sisestus.grid(row=i, column=j, padx=10, pady=10)
 
-arvamine_muutuja=tk.StringVar()
-arvamine_sisestus=tk.Entry(põhiraam, textvariable=arvamine_muutuja, font=('Tahoma', 24), width=7, bg="#928aff", fg="#87ceeb")
-arvamine_sisestus.grid(row=7, column=0, columnspan=5, padx=10, pady=10)
+saatmis=tk.Button(põhiraam, text="Saada", command=saatmine, bg="#928aff", fg="#87ceeb")
+saatmis.grid(row=6, column=0, columnspan=3, padx=5, pady=10)
 
-saatmis_nupp=tk.Button(põhiraam, text="Saada", command=saatmine, bg="#928aff", fg="#87ceeb")
-saatmis_nupp.grid(row=8, column=0, columnspan=3, padx=5, pady=10)
+jäänud=tk.Label(põhiraam, text="", font=('Tahoma', 24), bg="#87ceeb", fg="#ffffff")  
+jäänud.grid(row=7, column=0, columnspan=5, padx=10, pady=10)
 
 silt=tk.Label(põhiraam, text="", font=('Tahoma', 24), bg="#87ceeb", fg="#ffffff")  
-silt.grid(row=9, column=0, columnspan=5, padx=10, pady=10)
+silt.grid(row=8, column=0, columnspan=5, padx=10, pady=10)
 
-
-algus_uuesti_nupp=tk.Button(põhiraam, text="Alusta uuesti", command=algus, bg="#928aff", fg="#87ceeb")
-algus_uuesti_nupp.grid(row=8, column=1, columnspan=5, padx=5, pady=10)
+nupp=tk.Button(põhiraam, text="Alusta uuesti", command=algus, bg="#928aff", fg="#87ceeb")
+nupp.grid(row=6, column=1, columnspan=9, padx=5, pady=10)
 
 aken.mainloop()
